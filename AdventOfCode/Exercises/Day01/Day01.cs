7cd4e78d7a5fc5699501b02cfb370f0f63f2b848
ShukTak Woo <shuktak.woo@barnett-waddingham.co.uk>
2023-12-01 21:44:06 +0000

    Part 1

---
+++ AdventOfCode/Exercises/Day01/Day01.cs
@@ -0,0 +1,29 @@
+﻿namespace AdventOfCode.Exercises.Day01;
+internal class Day01 {
+
+    internal static void Run() {
+
+        var filePath = new Uri(new Uri(AppDomain.CurrentDomain.BaseDirectory), "Exercises\\Day01\\Data.txt");
+        var lines = File.ReadLines(filePath.AbsolutePath).ToList();
+
+        // Using foreach loop
+        int sum = 0;
+        foreach (var line in lines) {
+            var firstNumber = line.First(c => Char.IsNumber(c));
+            var lastNumber = line.Last(c => Char.IsNumber(c));
+            int number = int.Parse($"{firstNumber}{lastNumber}");
+            //Console.WriteLine($"Line is {line}: firstNumber is {firstNumber}: lastNumber is {lastNumber}: number is {number}: sum is {sum}");
+            sum += number;
+        }
+        Console.WriteLine($"Sum is {sum}");
+
+        // Using list foreach
+        int sumFromLinq = 0;
+        lines.ForEach(line => {
+            sumFromLinq += int.Parse($"{line.First(c => Char.IsNumber(c))}{line.Last(c => Char.IsNumber(c))}");
+        });
+        Console.WriteLine($"Sum (linq) is {sumFromLinq}");
+    }
+}
+
+
---
+++ AdventOfCode/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AdventOfCode.Exercises.Day01;
+
+Day01.Run();
