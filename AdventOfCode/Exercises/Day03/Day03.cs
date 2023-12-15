9e0c7d6a9e0f3ea153eb2a1b764e1a85496a6037
ShukTak Woo <shuktak.woo@barnett-waddingham.co.uk>
2023-12-15 00:09:13 +0000

    Day 3 - part 2 done

--- AdventOfCode/Exercises/Day03/Day03.cs
+++ AdventOfCode/Exercises/Day03/Day03.cs
@@ -1,25 +1,33 @@
-﻿using static AdventOfCode.Exercises.Day01.Day01;
+﻿using FluentAssertions;
+using FluentAssertions.Data;
+using System.Diagnostics.Metrics;
+using System.Numerics;
+using static AdventOfCode.Exercises.Day01.Day01;
 using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace AdventOfCode.Exercises.Day03;
 public class Day03 {
 
+    private readonly List<string> lines;
     private readonly Uri filePath = new(new Uri(AppDomain.CurrentDomain.BaseDirectory), "Exercises\\Day03\\Data.txt");
     readonly char[,] dataArray = default!;
+    readonly bool[,] usedArray = default!;
 
     public Day03() {
 
-        var lines = File.ReadLines(this.filePath.AbsolutePath).ToList();
-        var numberOfRows = lines.Count;
-        var numberOfColumns = lines.FirstOrDefault()?.Length;
+        this.lines = File.ReadLines(this.filePath.AbsolutePath).ToList();
+        var numberOfRows = this.lines.Count;
+        var numberOfColumns = this.lines.FirstOrDefault()?.Length;
         if (numberOfColumns > 0) {
             this.dataArray = new char[numberOfRows, numberOfColumns.Value];
-            for (int row = 0; row < lines.Count; row++) {
+            this.usedArray = new bool[numberOfRows, numberOfColumns.Value];
+            for (int row = 0; row < this.lines.Count; row++) {
 
-                var line = lines[row];
+                var line = this.lines[row];
 
                 for (int column = 0; column < line.Length; column++) {
                     this.dataArray[row, column] = line[column];
+                    this.usedArray[row, column] = false;
                 }
             }
         }
@@ -68,6 +76,8 @@ public class Day03 {
 
     public void RunPart2() {
 
+        long total = 0;
+        List<int> numberList = new();
         for (int row = 0; row < this.dataArray.GetLength(0); row++) {
 
             for (int column = 0; column < this.dataArray.GetLength(1); column++) {
@@ -75,8 +85,129 @@ public class Day03 {
                 var character = this.dataArray[row, column];
                 if (IsGear(character)) {
 
+                    int adjacentRow;
+                    int adjacentColumn;
+                    numberList.Clear();
+
+                    // Row above
+                    if (row > 0) {
+
+                        if (column > 0) {
+                            adjacentRow = row - 1;
+                            adjacentColumn = column - 1;
+                            if (this.IsNumber(adjacentRow, adjacentColumn)) {
+                                numberList.Add(this.ExtractNumber(adjacentRow, adjacentColumn));
+                            }
+                        }
+
+                        adjacentRow = row - 1;
+                        adjacentColumn = column;
+                        if (this.IsNumber(adjacentRow, adjacentColumn)) {
+                            numberList.Add(this.ExtractNumber(adjacentRow, adjacentColumn));
+                        }
+
+                        if (column < this.dataArray.GetLength(1)) {
+                            adjacentRow = row - 1;
+                            adjacentColumn = column + 1;
+                            if (this.IsNumber(adjacentRow, adjacentColumn)) {
+                                numberList.Add(this.ExtractNumber(adjacentRow, adjacentColumn));
+                            }
+                        }
+                    }
+
+                    // Same row
+                    if (column > 0) {
+                        adjacentRow = row;
+                        adjacentColumn = column - 1;
+                        if (this.IsNumber(adjacentRow, adjacentColumn)) {
+                            numberList.Add(this.ExtractNumber(adjacentRow, adjacentColumn));
+                        }
+                    }
+
+                    if (column < this.dataArray.GetLength(1)) {
+                        adjacentRow = row;
+                        adjacentColumn = column + 1;
+                        if (this.IsNumber(adjacentRow, adjacentColumn)) {
+                            numberList.Add(this.ExtractNumber(adjacentRow, adjacentColumn));
+                        }
+                    }
+
+                    // Row below
+                    if (row < this.dataArray.GetLength(0)) {
+                        if (column > 0) {
+                            adjacentRow = row + 1;
+                            adjacentColumn = column - 1;
+                            if (this.IsNumber(adjacentRow, adjacentColumn)) {
+                                numberList.Add(this.ExtractNumber(adjacentRow, adjacentColumn));
+                            }
+                        }
+                        adjacentRow = row + 1;
+                        adjacentColumn = column;
+                        if (this.IsNumber(adjacentRow, adjacentColumn)) {
+                            numberList.Add(this.ExtractNumber(adjacentRow, adjacentColumn));
+                        }
+                        if (column < this.dataArray.GetLength(1)) {
+                            adjacentRow = row + 1;
+                            adjacentColumn = column + 1;
+                            if (this.IsNumber(adjacentRow, adjacentColumn)) {
+                                numberList.Add(this.ExtractNumber(adjacentRow, adjacentColumn));
+                            }
+                        }
+                    }
+
+                    if (numberList.Count == 2) {
+                        total += (numberList[0] * numberList[1]);
+                    }
                 }
             }
+        }
+
+        Console.WriteLine($"PART 2");
+        Console.WriteLine($"Total is {total}");
+    }
+
+    private bool IsNumber(int row, int column) {
+        return (char.IsNumber(this.dataArray[row, column]) && !this.usedArray[row, column]);
+    }
+
+    private int ExtractNumber(int row, int column) {
+
+        var boundaryIndexes = this.FindNumberBoundaryIndexes(row, column);
+        this.MarkAsUsed(row, boundaryIndexes.startColumnIndex, boundaryIndexes.endColumnIndex);
+        return this.GetNumber(row, boundaryIndexes.startColumnIndex, boundaryIndexes.endColumnIndex);
+    }
+
+    private (int startColumnIndex, int endColumnIndex) FindNumberBoundaryIndexes(int row, int column) {
+
+        int endColumnIndex = column;
+        while (endColumnIndex < this.dataArray.GetLength(1) && this.IsNumber(row, endColumnIndex) ) {
+            endColumnIndex++;
+        }
+        int startColumnIndex = column - 1;
+        while (startColumnIndex >= 0 && this.IsNumber(row, startColumnIndex)) {
+            startColumnIndex--;
+        }
+
+        startColumnIndex++;
+        endColumnIndex--;
+        return (startColumnIndex, endColumnIndex);
+    }
+
+    private int GetNumber(int row, int startColumnIndex, int endColumnIndex) {
+
+        string numberText = string.Empty;
+        for (int column = startColumnIndex; column <= endColumnIndex; column++) {
+
+            numberText += this.dataArray[row, column];
+        }
+        return int.Parse(numberText);
+    }
+
+    private void MarkAsUsed(int row, int startColumnIndex, int endColumnIndex) {
+
+        for (int column = startColumnIndex; column <= endColumnIndex; column++) {
+
+            this.usedArray[row, column] = true;
         }
     }
 
@@ -117,6 +248,62 @@ public class Day03 {
     private bool IsAboveRightSymbol(int currentRow, int currentColumn) => (currentRow > 0 && currentColumn < this.dataArray.GetLength(1) - 1) && IsSymbol(this.dataArray[currentRow - 1, currentColumn + 1]);
     private bool IsLeftSymbol(int currentRow, int currentColumn) => (currentColumn > 0) && IsSymbol(this.dataArray[currentRow, currentColumn - 1]);
     private bool IsRightSymbol(int currentRow, int currentColumn) => (currentColumn < this.dataArray.GetLength(1) - 1) && IsSymbol(this.dataArray[currentRow, currentColumn + 1]);
+
+    //private bool IsBottomNumber(int currentRow, int currentColumn) => (currentRow < this.dataArray.GetLength(0) - 1) && char.IsNumber((this.dataArray[currentRow + 1, currentColumn]));
+    //private bool IsBottomLeftNumber(int currentRow, int currentColumn) => (currentRow < this.dataArray.GetLength(0) - 1 && currentColumn > 0) && char.IsNumber((this.dataArray[currentRow + 1, currentColumn - 1]));
+    //private bool IsBottomRightNumber(int currentRow, int currentColumn) => (currentRow < this.dataArray.GetLength(0) - 1 && currentColumn < this.dataArray.GetLength(1) - 1) && char.IsNumber((this.dataArray[currentRow + 1, currentColumn + 1]));
+    //private bool IsAboveNumber(int currentRow, int currentColumn) => (currentRow > 0) && char.IsNumber((this.dataArray[currentRow - 1, currentColumn]));
+    //private bool IsAboveLeftNumber(int currentRow, int currentColumn) => (currentRow > 0 && currentColumn > 0) && char.IsNumber((this.dataArray[currentRow - 1, currentColumn - 1]));
+    //private bool IsAboveRightNumber(int currentRow, int currentColumn) => (currentRow > 0 && currentColumn < this.dataArray.GetLength(1) - 1) && char.IsNumber((this.dataArray[currentRow - 1, currentColumn + 1]));
+    //private bool IsLeftNumber(int currentRow, int currentColumn) => (currentColumn > 0) && char.IsNumber((this.dataArray[currentRow, currentColumn - 1]));
+    //private bool IsRightNumber(int currentRow, int currentColumn) => (currentColumn < this.dataArray.GetLength(1) - 1) && char.IsNumber((this.dataArray[currentRow, currentColumn + 1]));
+
+    private (bool isNumber, char character) IsBottomNumber(int currentRow, int currentColumn) {
+        var character = this.dataArray[currentRow + 1, currentColumn];
+        var isNumber = (currentRow < this.dataArray.GetLength(0) - 1) && char.IsNumber(character);
+        return (isNumber, character);
+    }
+
+    private (bool isNumber, char character) IsBottomLeftNumber(int currentRow, int currentColumn) {
+        var character = this.dataArray[currentRow + 1, currentColumn - 1];
+        var isNumber = (currentRow < this.dataArray.GetLength(0) - 1 && currentColumn > 0) && char.IsNumber(character);
+        return (isNumber, character);
+    }
+
+    private (bool isNumber, char character) IsBottomRightNumber(int currentRow, int currentColumn) {
+        var character = this.dataArray[currentRow + 1, currentColumn + 1];
+        var isNumber = (currentRow < this.dataArray.GetLength(0) - 1 && currentColumn < this.dataArray.GetLength(1) - 1) && char.IsNumber(character);
+        return (isNumber, character);
+    }
+    private (bool isNumber, char character) IsAboveNumber(int currentRow, int currentColumn) {
+        var character = this.dataArray[currentRow - 1, currentColumn];
+        var isNumber = (currentRow > 0) && char.IsNumber(character);
+        return (isNumber, character);
+    }
+
+    private (bool isNumber, char character) IsAboveLeftNumber(int currentRow, int currentColumn) {
+        var character = this.dataArray[currentRow - 1, currentColumn - 1];
+        var isNumber = (currentRow > 0 && currentColumn > 0) && char.IsNumber(character);
+        return (isNumber, character);
+    }
+
+    private (bool isNumber, char character) IsAboveRightNumber(int currentRow, int currentColumn) {
+        var character = this.dataArray[currentRow - 1, currentColumn + 1];
+        var isNumber = (currentRow > 0 && currentColumn < this.dataArray.GetLength(1) - 1) && char.IsNumber(character);
+        return (isNumber, character);
+    }
+
+    private (bool isNumber, char character) IsLeftNumber(int currentRow, int currentColumn) {
+        var character = this.dataArray[currentRow, currentColumn - 1];
+        var isNumber = (currentColumn > 0) && char.IsNumber(character);
+        return (isNumber, character);
+    }
+
+    private (bool isNumber, char character) IsRightNumber(int currentRow, int currentColumn) {
+        var character = this.dataArray[currentRow, currentColumn + 1];
+        var isNumber = (currentColumn < this.dataArray.GetLength(1) - 1) && char.IsNumber(character);
+        return (isNumber, character);
+    }
 
     #endregion
 }
--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -2,7 +2,7 @@
 using System.Diagnostics;
 using System.Reflection;
 
-int day = 10;
+int day = 3;
 
 string className = $"Day{day.ToString().PadLeft(2, '0')}";
 Console.WriteLine(className);
